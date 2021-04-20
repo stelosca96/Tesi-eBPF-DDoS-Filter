a548b960cc3d1d0ee7b6f1e6850a8cea703991c7
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2021-04-20 15:57:31 +0200

    flows

---
+++ counter.c
@@ -0,0 +1,94 @@
+#define KBUILD_MODNAME "filter"
+#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/in.h>
+#include <linux/udp.h>
+#include <linux/tcp.h>
+
+//struct counter_data {
+//    u32 syn_tx;
+//    u32 rst_tx;
+//    u32 fin_tx;
+//    u32 udp_tx;
+//    u32 icmp_tx;
+//    u32 tcp_tx;
+//    u32 packet_rate_tx;
+//    u32 throughput_tx;
+//};
+
+// per creare una mappa che usa int per maggiore efficienza posso usare un u64 con
+// primi 16bit porta dst, secondi 16 bit 0.0.x.x ultimi
+// valori dell'ip escludo il prefisso, ipotizzo di usare al masimo una /16,
+// ultimi 32bit ip dst
+BPF_HISTOGRAM(syn_tx_counter_by_src, u64, 1024);
+BPF_HISTOGRAM(rst_tx_counter_by_src, u64, 1024);
+BPF_HISTOGRAM(fin_tx_counter_by_src, u64, 1024);
+BPF_HISTOGRAM(udp_tx_counter_by_src, u64, 1024);
+BPF_HISTOGRAM(icmp_tx_counter_by_src, u64, 1024);
+BPF_HISTOGRAM(tcp_tx_counter_by_src, u64, 1024);
+BPF_HISTOGRAM(packet_rate_tx_counter_by_src, u64, 1024);
+//BPF_HISTOGRAM(throughput_tx_counter_by_src, u64, 1024);
+
+// name, key size, value size, table size
+// scegliere un valore ragionevole per la dimensione della tabella
+BPF_HASH(blacklist_table, u64, bool, 10240);
+
+static u64 get_map_key(u32 src_ip, u32 dst_ip, u16 dst_port){
+    u64 value = dst_ip;
+    value += ((u64) (src_ip & 0xFFFF0000) << 16);
+    value += ((u64) dst_port << 48);
+    return value;
+};
+
+
+int filter(struct xdp_md *ctx) {
+//  bpf_trace_printk("ddos filter\n");
+  void *data = (void *)(long)ctx->data;
+  void *data_end = (void *)(long)ctx->data_end;
+  struct ethhdr *eth = data;
+  if ((void*)eth + sizeof(*eth) <= data_end) {
+    struct iphdr *ip = data + sizeof(*eth);
+    if ((void*)ip + sizeof(*ip) <= data_end) {
+      if (ip->protocol == IPPROTO_UDP) {
+        struct udphdr *udp = (void*)ip + sizeof(*ip);
+        if ((void*)udp + sizeof(*udp) <= data_end) {
+            u64 key = get_map_key(ip->saddr, ip->daddr, udp->dest);
+            packet_rate_tx_counter_by_src.increment(key);
+            udp_tx_counter_by_src.increment(key);
+//          if (udp->dest == ntohs(7999)) {
+//            // bpf_trace_printk("udp port 7999\n");
+//            udp->dest = ntohs(7998);
+//          }
+        }
+      }
+      if (ip->protocol == IPPROTO_TCP){
+        struct tcphdr *tcp = (void*)ip + sizeof(*ip);
+        if ((void*)tcp + sizeof(*tcp) <= data_end) {
+            u64 key = get_map_key(ip->saddr, ip->daddr, tcp->dest);
+            packet_rate_tx_counter_by_src.increment(key);
+            tcp_tx_counter_by_src.increment(key);
+            if (tcp->syn) {
+                bpf_trace_printk("syn packet: %lx %lx\n", tcp->dest, key);
+                syn_tx_counter_by_src.increment(key);
+                // sea una delle due chiavi è presente nella blacklist faccio il drop del pacchetto
+                // le blacklist sono riempite dall'interfaccia in python ogni x secondi
+//                if(blacklist_table.lookup(&key)!=NULL){
+//                    bpf_trace_printk("blacklist\n");
+//                    return XDP_DROP;
+//                }
+            }
+            else if (tcp->fin) {
+                bpf_trace_printk("fin packet\n");
+                fin_tx_counter_by_src.increment(key);
+            }
+            else if (tcp->rst){
+                bpf_trace_printk("rst packet\n");
+                rst_tx_counter_by_src.increment(key);
+            }
+        }
+      }
+    }
+  }
+  return XDP_PASS;
+}
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import ctypes
+from typing import Dict
+
 from bcc import BPF
 from bcc.table import HashTable
 from bcc import BPF
@@ -8,6 +11,20 @@ from socket import ntohl, ntohs
 from ctypes import c_ulong, c_bool
 from db import Db
 import os
+
+features = ['syn_tx', 'rst_tx', 'fin_tx', 'udp_tx',
+            'icmp_tx', 'tcp_tx', 'packet_rate_tx']
+# class CounterData(ctypes.Structure):
+#     _fields_ = [
+#         ("syn_tx", ctypes.c_uint),
+#         ("rst_tx", ctypes.c_uint),
+#         ("fin_tx", ctypes.c_uint),
+#         ("udp_tx", ctypes.c_uint),s
+#         ("icmp_tx", ctypes.c_uint),
+#         ("tcp_tx", ctypes.c_uint),
+#         ("packet_rate_tx", ctypes.c_uint),
+#         ("throughput_tx", ctypes.c_uint),
+#     ]
 
 
 # conto i syn già considerati anomali,
@@ -26,13 +43,12 @@ def add_anomaly(ip, port, syn, fin):
 
 
 def clear_maps():
-    syn_dst.clear()
-    fin_dst.clear()
-    syn_src.clear()
-    fin_src.clear()
-    rst_src.clear()
+    # syn_dst.clear()
+    # fin_dst.clear()
     blacklist.clear()
     anomaly_by_ip_port.clear()
+    for i in features:
+        tables[i].clear()
 
 
 def get_anomaly_syn_fin(ip, port):
@@ -43,70 +59,77 @@ def get_anomaly_syn_fin(ip, port):
 
 
 device = "enp0s3"
-b = BPF(src_file="filter.c")
+b = BPF(src_file="counter.c")
 fn = b.load_func("filter", BPF.XDP)
 b.attach_xdp(device, fn, 0)
 
 anomaly_by_ip_port = dict()
 blacklist = set()
 
-syn_dst: HashTable = b.get_table("syn_counter_by_dst")
-fin_dst: HashTable = b.get_table("fin_counter_by_dst")
-syn_src: HashTable = b.get_table("syn_counter_by_src")
-fin_src: HashTable = b.get_table("fin_counter_by_src")
-rst_src: HashTable = b.get_table("rst_counter_by_src")
+# syn_dst: HashTable = b.get_table("syn_counter_by_dst")
+# fin_dst: HashTable = b.get_table("fin_counter_by_dst")
+
+tables: Dict[str, HashTable] = dict()
+for feature in features:
+    key = f"{feature}_counter_by_src"
+    print(key)
+    tables[feature]: HashTable = b.get_table(key)
+
 db = Db('root', 'ciao12345', 'anomaly_detection', '192.168.1.20')
 blacklist_table: HashTable = b.get_table("blacklist_table")
+
 try:
     # b.trace_print()
     while True:
         print('get data')
         blacklist_table.clear()
-
-        for k, v in syn_src.items():
-            syn_count = v.value
-            fin_count = fin_src.get(k).value if fin_src.get(k) is not None else 0
-            rst_count = rst_src.get(k).value if rst_src.get(k) is not None else 0
-            dst_port = ntohs((k.value & 0xFFFF000000000000) >> 48)
-            dst_ip = IPv4Address(ntohl((k.value & 0xFFFFFFFF)))
-            src_ip = IPv4Address(ntohl((k.value & 0xFFFF00000000) >> 16))
-            data = {
+        for k, v in tables['packet_rate_tx'].items():
+            data = dict()
+            data['packet_rate_tx'] = v.value
+            for feature in features:
+                data[feature] = tables[feature].get(k).value if tables[feature].get(k) is not None else 0
+            data.update({
                 'ip_src': ntohl((k.value & 0xFFFF00000000) >> 16),
                 'ip_dst': ntohl((k.value & 0xFFFFFFFF)),
-                'port_dst': ntohs((k.value & 0xFFFF000000000000) >> 48),
-                'syn_tx': syn_count,
-                'rst_tx': rst_count,
-                'fin_tx': fin_count,
-            }
+                'port_dst': ntohs((k.value & 0xFFFF000000000000) >> 48)
+            })
+
             db.add_data(data)
             print("dest ip: %15s:%4d, src ip: %3s, syn_count: %3d, fin_count: %3d, rst_count: %3d" %
-                  (dst_ip, dst_port, src_ip, syn_count, fin_count, rst_count))
+                  (
+                      IPv4Address(data["ip_dst"]),
+                      data["port_dst"],
+                      IPv4Address(data["ip_src"]),
+                      data["syn_tx"],
+                      data["fin_tx"],
+                      data["rst_tx"])
+                  )
             # todo: scegliere una soglia
-            if syn_count > 10 and syn_count/(syn_count+fin_count+rst_count) > 0.7:
-                blacklist.add(f"{src_ip} =>a {dst_ip}:{dst_port}")
-                blacklist_table[k] = c_bool(True)
-                add_anomaly(dst_ip, dst_port, syn_count, fin_count+rst_count)
+            # if syn_count > 10 and syn_count/(syn_count+fin_count+rst_count) > 0.7:
+            #     blacklist.add(f"{src_ip} =>a {dst_ip}:{dst_port}")
+            #     blacklist_table[k] = c_bool(True)
+            #     add_anomaly(dst_ip, dst_port, syn_count, fin_count+rst_count)
 
-        for k, v in syn_dst.items():
-            syn_count = v.value
-            fin_count = fin_dst.get(k).value if fin_dst.get(k) is not None else 0
-            dst_port = ntohs((k.value & 0xFFFF000000000000) >> 48)
-            dst_ip = IPv4Address(ntohl((k.value & 0xFFFFFFFF)))
-            # print(fin_count)
-            print("dest ip: %15s:%4d, syn_count: %3d, fin_count: %3d" %
-                  (dst_ip, dst_port, syn_count, fin_count))
-            # sottraggo i syn e i fin già considerati un'anomalia
-            s, f = get_anomaly_syn_fin(dst_ip, dst_port)
-            syn_count -= s
-            fin_count -= f
-            if syn_count > 10 and syn_count / (syn_count + fin_count) > 0.7:
-                blacklist.add(f"{dst_ip}:{dst_port}")
-                blacklist_table[k] = c_bool(True)
-                print("new: dest ip: %15s:%4d, syn_count: %3d, fin_count: %3d" %
-                      (dst_ip, dst_port, syn_count, fin_count))
-
-        print(len(syn_dst.items()))
-        print(len(syn_src.items()))
+        # for k, v in syn_dst.items():
+        #     syn_count = v.value
+        #     fin_count = fin_dst.get(k).value if fin_dst.get(k) is not None else 0
+        #     dst_port = ntohs((k.value & 0xFFFF000000000000) >> 48)
+        #     dst_ip = IPv4Address(ntohl((k.value & 0xFFFFFFFF)))
+        #     # print(fin_count)
+        #     print("dest ip: %15s:%4d, syn_count: %3d, fin_count: %3d" %
+        #           (dst_ip, dst_port, syn_count, fin_count))
+        #     # sottraggo i syn e i fin già considerati un'anomalia
+        #     s, f = get_anomaly_syn_fin(dst_ip, dst_port)
+        #     syn_count -= s
+        #     fin_count -= f
+        #     if syn_count > 10 and syn_count / (syn_count + fin_count) > 0.7:
+        #         blacklist.add(f"{dst_ip}:{dst_port}")
+        #         blacklist_table[k] = c_bool(True)
+        #         print("new: dest ip: %15s:%4d, syn_count: %3d, fin_count: %3d" %
+        #               (dst_ip, dst_port, syn_count, fin_count))
+        #
+        # print(len(syn_dst.items()))
+        # print(len(syn_src.items()))
         print(blacklist)
         clear_maps()
         # todo: pulire mappe
